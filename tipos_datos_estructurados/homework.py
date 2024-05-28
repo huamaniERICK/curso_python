7842dd0eb972d5bd60be30112e4c39e697252782
huamaniERICK <erickhuamaniccahuay10@gmail.com>
2024-05-28 17:07:28 -0500

    actualize mi trabajo

--- tipos_datos_estructurados/homework.py
+++ tipos_datos_estructurados/homework.py
@@ -8,13 +8,15 @@ lista_alumnos=[{
     "apellidos":"huamani",
     "edad":20,
 },{
-    "nombre":"juan",
-    "apellidos":"lopez",
-    "edad":24,
-},{
     "nombre":"abel",
     "apellidos":"llamocca",
     "edad":25,
+    
+},{
+    "nombre":"juan",
+    "apellidos":"lopez",
+    "edad":24,
+    
 },{
     "nombre":"daniel",
     "apellidos":"huamani",
@@ -23,16 +25,29 @@ lista_alumnos=[{
 
 ]
 # insertar al final los datos de antoni
-lista_nueva=[]
-lista_nueva.append(
+lista_nueva=[{
     "nombres":"antoni",
     "apellidos":"isase",
     "edad":21,
-)
-print(lista_nueva)
+}]
+lista_alumnos.append(lista_nueva)
+print(lista_alumnos)
 
 # eliminar de la lista si existe los datos de abel
-lista_alumnos=[
-    lista_alumnos.remove("nombre","a")]
+
+
 
 # buscar y mostrar el alumno en la pocicion 4
+lista_alumnos=[{
+    "nombre":"abel",
+    "apellidos":"llamocca",
+    "edad":25,
+}
+]
+indice=lista_alumnos.index[{
+    "nombre":"abel",
+    "apellidos":"llamocca",
+    "edad":25,
+}
+]
+print(lista_alumnos[indice])
